55415f2d74d08f84f52208029863e958667a480d
rahmabenomrane <rahmabenomrane5@gmail.com>
2025-12-07 02:15:04 +0100

    Fix SonarQube issues and updates

--- cotebackend/config/connect.js
+++ cotebackend/config/connect.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/pfe')
-.then(
-    ()=>{
-     console.log('connected')
-    }
-)
-.catch(
-    (err)=>{
-       console.log('error')
-    }
-)
-module.exports == mongoose;
+mongoose
+  .connect("mongodb://127.0.0.1:27017/pfe")
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log("error");
+  });
+module.exports = mongoose;
--- cotebackend/middleware/YoutubeApi.js
+++ cotebackend/middleware/YoutubeApi.js
@@ -1,27 +1,30 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 // Set up YouTube API client
 const youtube = google.youtube({
-  version: 'v3',
-  auth: ''
+  version: "v3",
+  auth: "AIzaSyDw6CP54PZ1ISALxFRxZnEhVBg_87tx3bA",
 });
 
 async function videoExists(videoId) {
   return new Promise((resolve, reject) => {
-    youtube.videos.list({
-      id: videoId,
-      part: 'id'
-    }, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        const videos = res.data.items;
-        if (videos.length > 0) {
-          resolve(true);
+    youtube.videos.list(
+      {
+        id: videoId,
+        part: "id",
+      },
+      (err, res) => {
+        if (err) {
+          reject(err);
         } else {
-          resolve(false);
+          const videos = res.data.items;
+          if (videos.length > 0) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
         }
       }
-    });
+    );
   });
 }
 module.exports = { videoExists };
